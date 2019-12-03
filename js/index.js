facebe47540e90f1d6a0ef6803aa5d145c93af82
Jacob Bremer <bremerjp@gmail.com>
2019-12-03 15:41:59 -0500

    added 3 more event listeners

--- js/index.js
+++ js/index.js
@@ -15,4 +15,48 @@ logo.addEventListener("mouseleave", () => {
     logo.style.transition = "transform 0.5s";
 })
 
-//
+// 2. Increase text size and add underline when nav items are clicked
+
+const navItems = document.querySelectorAll("nav a");
+
+navItems.forEach(el => {
+    el.addEventListener("click", () => {
+        el.style.transform = "scale(1.2)";
+        el.style.transition = "transform 0.4s";
+        el.style.borderBottom = "1px solid black";
+    })
+  })
+
+  navItems.forEach(el => {
+    el.addEventListener("mouseleave", () => {
+        el.style.transform = "scale(1)";
+        el.style.transition = "transform 0.4s";
+        el.style.borderBottom = "";
+    })
+  })
+
+  // 3. double click event on bus image
+
+  const busImage = document.querySelector("header img");
+  
+  busImage.addEventListener("dblclick", () => {
+      busImage.style.transform = "rotate(360deg)";
+      busImage.style.transition = "transform 1s";
+  })
+
+  // 4. change color of headers on scroll
+
+  const h2 = document.querySelectorAll("h2");
+  
+  h2.forEach(el => {
+      window.addEventListener("scroll", () => {
+        el.style.color = "goldenrod";
+      })
+  })
+
+  // 5. 
+
+
+  
+
+
