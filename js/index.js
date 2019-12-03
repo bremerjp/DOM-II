fa45a12bf87c8ba857cbc1a42df10a55319143a7
Jacob Bremer <bremerjp@gmail.com>
2019-12-03 14:58:32 -0500

    added event listener on logo text

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+
+// - 1. Increase logo font size on hover 
+
+const logo = document.querySelector(".logo-heading");
+
+logo.addEventListener("mouseenter", () => {
+    logo.style.transform = "scale(1.4)";
+    logo.style.transition = "transform 0.4s";
+})
+
+logo.addEventListener("mouseleave", () => {
+    logo.style.transform = "scale(1)";
+    logo.style.transition = "transform 0.5s";
+})
+
+//
